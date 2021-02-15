d86399a1540516153c56cf7c5e6cb43939e81aed
oncetrader2018 <oncetrader2018@gmail.com>
2021-02-15 23:28:31 +0800

    继承

---
+++ Java/src/com/cjj/java06/Dog.java
@@ -0,0 +1,51 @@
+package com.cjj.java06;
+
+public class Dog extends Pet{
+    //私有属性
+//    private String name;
+//    private int age;
+//    private String gender;
+    private String sound;
+
+    //构造方法
+    public Dog(){}
+
+    public Dog(String name, int age, String gender, String sound) {
+        super(name,age,gender);
+        this.sound = sound;
+    }
+
+
+    //get and set
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getGender() {
+//        return gender;
+//    }
+//
+//    public void setGender(String gender) {
+//        this.gender = gender;
+//    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
---
+++ Java/src/com/cjj/java06/PetTest.java
@@ -0,0 +1,11 @@
+package com.cjj.java06;
+
+public class PetTest {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.setName("小黄");
+        System.out.println(dog.getName());
+
+    }
+}
