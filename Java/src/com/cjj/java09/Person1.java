c83491a2fdd3aaf304225dee47e346c6fbef8160
oncetrader2018 <oncetrader2018@gmail.com>
2021-02-28 19:58:09 +0800

    父类作为方法的参数 + 父类作为方法的返回值类型

--- Java/src/com/cjj/java09/Person1.java
+++ Java/src/com/cjj/java09/Person1.java
@@ -7,6 +7,8 @@ package com.cjj.java09;
 
 //思路：先写父类，再写子类，在子类中重写父类的方法，然后在使用时就使用父类的类型，子类的对象，这时会自动类型转换
 
+import com.cjj.java08.Car;
+
 public class Person1 {
 
 /*
@@ -21,8 +23,20 @@ public class Person1 {
 */
 
     //----写它的父类，dog和cat的父类都是Pet
+    //父类作为方法的参数
     public void feed(Pet pet){
         pet.feed();
+    }
+
+    //父类作为方法的返回值类型
+    public Pet buyPet(int type){
+        if(type == 1){
+            return new Dog();
+        }else if(type == 2){
+            return new Cat();
+        }else{
+            return new Pig();
+        }
     }
 
     public static void main(String[] args) {
@@ -40,5 +54,15 @@ public class Person1 {
         p.feed(cat);
         //再去喂猪
         p.feed(pig);
+
+
+        Pet pet = p.buyPet(1);
+        if(pet instanceof Dog){
+            System.out.println("买了狗");
+        }else if (pet instanceof Cat){
+            System.out.println("买了猫");
+        }else {
+            System.out.println("买了猪");
+        }
     }
 }
